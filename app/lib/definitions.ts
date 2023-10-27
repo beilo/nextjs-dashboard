91f7d9f981d95995f472e93bb4050b32407f60ac
雷鹏 <leipeng@codemao.cn>
2023-10-27 16:17:25 +0800

    init

---
+++ app/lib/definitions.ts
@@ -0,0 +1,88 @@
+// 此文件包含数据类型定义。
+// 它描述了数据的形状，以及每个属性应该接受的数据类型。
+// 为了教学的简单性，我们手动定义这些类型。
+// 但是，如果您使用像 Prisma 这样的 ORM，则这些类型会自动生成。
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type Customer = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+};
+
+export type Invoice = {
+  id: string;
+  customer_id: string;
+  amount: number;
+  date: string;
+  // 在 TypeScript 中，这被称为字符串联合类型。
+  // 这意味着“status”属性只能是两个字符串之一：“pending”或“paid”。
+  status: 'pending' | 'paid';
+};
+
+export type Revenue = {
+  month: string;
+  revenue: number;
+};
+
+export type LatestInvoice = {
+  id: string;
+  name: string;
+  image_url: string;
+  email: string;
+  amount: string;
+};
+
+// 数据库返回的是一个数字，但我们稍后会使用 formatCurrency 函数将其格式化为字符串。
+export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
+  amount: number;
+};
+
+export type InvoicesTable = {
+  id: string;
+  customer_id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  date: string;
+  amount: number;
+  status: 'pending' | 'paid';
+};
+
+export type CustomersTable = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  total_invoices: number;
+  total_pending: number;
+  total_paid: number;
+};
+
+export type FormattedCustomersTable = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  total_invoices: number;
+  total_pending: string;
+  total_paid: string;
+};
+
+export type CustomerField = {
+  id: string;
+  name: string;
+};
+
+export type InvoiceForm = {
+  id: string;
+  customer_id: string;
+  amount: number;
+  status: 'pending' | 'paid';
+};
